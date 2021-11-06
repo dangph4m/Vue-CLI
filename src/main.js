1725224ce225360f4a14f4dbade3b48a884e1317
Dang Pham <phamthaodang123@gmail.com>
2021-11-06 13:38:28 +0700

    Lab 5

--- src/main.js
+++ src/main.js
@@ -1,19 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from "./store";
 import { handle } from "./common/promise";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-//createApp(App).use(router).mount('#app');
-//createApp(App)
-//    .mixin({
-//        methods: {
-//                handle,
-//        }
-//    })
-//    .mount('#app');
-const app=createApp(App);
-app.use(router);
-app.mixin({methods: {handle}});
-app.mount("#app");
+createApp(App)
+    .mixin({
+        methods: {
+            handle,
+        },
+    })
+    .use(store)
+    .use(router)
+    .mount("#app");
